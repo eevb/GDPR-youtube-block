1f836f78258f2c4a978d89dcab6a795e781af1ac
eevb <96337431+eevb@users.noreply.github.com>
2022-03-02 00:28:29 +0200

    Create GDPR.js

---
+++ GDPR.js
@@ -0,0 +1,7 @@
+//block GDPR
+/// youtugegdpr.js
+/// alias gdpr.js
+window.addEventListener('DOMContentLoaded',function () {
+  element = document.querySelector("button[aria-label]").click();
+  console.log('Works!!');
+});
