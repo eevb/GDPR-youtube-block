b9d29cfbbecad25b78cbe90c0916bf732921556c
eevb <96337431+eevb@users.noreply.github.com>
2022-06-09 13:57:41 +0300

    Create gdpr-youtube.js

---
+++ gdpr-youtube.js
@@ -0,0 +1,9 @@
+/// youtube-remove-gdpr.js
+/// alias gdprr.js
+
+ window.addEventListener('DOMContentLoaded', myFunct());
+
+  function myFunct() {
+    element = document.querySelector('[aria-label="Reject all"]').click();
+    console.log('test1');
+  }
